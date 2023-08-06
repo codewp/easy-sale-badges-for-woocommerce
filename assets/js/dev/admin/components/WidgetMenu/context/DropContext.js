e9e9d13ee60789c621bc6fe22593686d3da3f4fa
prswzz <pzmgk10@gmail.com>
2023-08-06 14:45:27 +0330

    Fix NavMenu

--- assets/js/dev/admin/components/WidgetMenu/context/DropContext.js
+++ assets/js/dev/admin/components/WidgetMenu/context/DropContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 export const DropContext = createContext();
 
 export const DropProvider = (props) => {
-  const [activeMenu, setActiveMenu] = useState("cssBadges");
+  const [activeMenu, setActiveMenu] = useState("cssLabel");
 
   return (
     <DropContext.Provider value={[activeMenu, setActiveMenu]}>
