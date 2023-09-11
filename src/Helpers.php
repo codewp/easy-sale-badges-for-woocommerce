51f3ea6b560249739c257089bed6eb3ca8a51d9c
prswzz <pzmgk10@gmail.com>
2023-09-11 15:23:22 +0330

    Fix some Themes Archieve Page

--- src/Helpers.php
+++ src/Helpers.php
@@ -196,7 +196,12 @@ function get_theme_loop_position( $stylesheet = null, $template = null ) {
 	$template   = ! empty( $template ) ? strtolower( $template ) : $template;
 
 	$themes = [
+		'avada' => 'after_shop_loop_item_thumbnail',
 		'woodmart' => 'after_shop_loop_item_thumbnail',
+		'porto' => 'after_shop_loop_item_thumbnail',
+		'oceanwp' => 'after_shop_loop_item_thumbnail',
+		'basel' => 'after_shop_loop_item_thumbnail',
+		'basel-child' => 'after_shop_loop_item_thumbnail',
 	];
 
 	if ( ! empty( $stylesheet ) && isset( $themes[ $stylesheet ] ) ) {
