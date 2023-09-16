a0cd2b7fa76468ceef40f2822933476245af4620
Taher Atashbar <tah3r.a@gmail.com>
2023-09-16 21:57:04 +0800

    Add action links to the plugin. Add black friday check function.

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -17,11 +17,12 @@ class Admin {
 
 	public function init() {
 		$this->register_dependencies();
+		$this->black_friday();
 
 		$this->container->get( Assets::class )->init();
 		$this->container->get( Menu::class )->init();
 
-		add_filter( 'plugin_row_meta', array( $this, 'plugin_row_meta_links' ), 10, 2 );
+		add_filter( 'plugin_action_links', array( $this, 'plugin_action_links' ), 10, 2 );
 	}
 
 	protected function register_dependencies() {
@@ -64,4 +65,65 @@ class Admin {
 
 		return $links;
 	}
+
+	/**
+	 * Plugin action links
+	 * This function adds additional links below the plugin in admin plugins page.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @param  array  $links    The array having default links for the plugin.
+	 * @param  string $file     The name of the plugin file.
+	 *
+	 * @return array  $links    Plugin default links and specific links.
+	 */
+	public function plugin_action_links( $links, $file ) {
+		if ( false === strpos( $file, 'easy-sale-badges.php' ) ) {
+			return $links;
+		}
+
+		$extra = [ '<a href="' . admin_url( 'admin.php?page=asnp-easy-sale-badge#/settings' ) . '">' . esc_html__( 'Settings', 'asnp-easy-sale-badge' ) . '</a>' ];
+
+		if ( ! SaleBadges\is_pro_active() ) {
+			$extra[] = '<a href="https://www.asanaplugins.com/product/woocommerce-sale-badges/?utm_source=sale-badges-for-woocommerce&utm_campaign=go-pro&utm_medium=link" target="_blank" onMouseOver="this.style.color=\'#55ce5a\'" onMouseOut="this.style.color=\'#39b54a\'" style="color: #39b54a; font-weight: bold;">' . esc_html__( 'Go Pro', 'asnp-easy-sale-badge' ) . '</a>';
+		}
+
+		return array_merge( $links, $extra );
+	}
+
+	protected function black_friday() {
+		if ( SaleBadges\is_pro_active() ) {
+			return;
+		}
+
+		$name = 'asnp_wesb_black_friday_' . date( 'Y' );
+		if ( (int) get_option( $name . '_added' ) ) {
+			// Is Black Friday expired.
+			if ( time() > strtotime( date( 'Y' ) . '-11-30' ) ) {
+				\WC_Admin_Notices::remove_notice( $name );
+				delete_option( $name . '_added' );
+			}
+			return;
+		}
+
+		if ( \WC_Admin_Notices::has_notice( $name ) ) {
+			return;
+		}
+
+		// Is Black Friday applicable.
+		if (
+			time() < strtotime( date( 'Y' ) . '-11-00' ) ||
+			time() > strtotime( date( 'Y' ) . '-11-30' )
+		) {
+			return;
+		}
+
+		\WC_Admin_Notices::add_custom_notice(
+			$name,
+			'<p>' . __( '<strong>Black Friday Exclusive:</strong> SAVE up to 50% & access to <strong>Sale Badges for WooCommerce PRO</strong> features.', 'asnp-easy-sale-badge' ) . '<a class="button button-primary" style="margin-left: 10px; background: #5614d5; border-color: #5614d5;" target="_blank" href="https://asanaplugins.com/product/woocommerce-sale-badges/?utm_source=sale-badges-for-woocommerce&utm_campaign=black-friday&utm_medium=link">' . __( 'Grab The Offer', 'asnp-easy-sale-badge' ) . '</a></p>'
+		);
+
+		update_option( $name . '_added', 1 );
+	}
+
 }
