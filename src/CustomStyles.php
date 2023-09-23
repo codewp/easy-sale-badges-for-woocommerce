6b86b6947992719cf97d2ed313513c7e473d6cf9
Taher Atashbar <tah3r.a@gmail.com>
2023-09-23 21:33:42 +0800

    Remove checking ! empty( $this->styles ).

--- src/CustomStyles.php
+++ src/CustomStyles.php
@@ -25,11 +25,7 @@ class CustomStyles {
 			return;
 		}
 
-		if ( empty( $this->styles ) ) {
-			$this->styles .= $style;
-		} else {
-			$this->styles .= ' ' . $style;
-		}
+		$this->styles .= $style;
 	}
 
 	public function output_styles() {
