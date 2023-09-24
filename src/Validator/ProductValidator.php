7fa464d099777c76d102513e2099e518c8613d01
Taher Atashbar <tah3r.a@gmail.com>
2023-09-24 20:41:46 +0800

    Fix class name.

--- src/Validator/ProductValidator.php
+++ src/Validator/ProductValidator.php
@@ -4,8 +4,8 @@ namespace AsanaPlugins\WooCommerce\SaleBadges\Validator;
 
 defined( 'ABSPATH' ) || exit;
 
-if ( class_exists( '\AsanaPlugins\WooCommerce\SaleBadgesPro\Validator\ProductValidatorPro' ) ) {
-	class ProductValidator extends \AsanaPlugins\WooCommerce\SaleBadgesPro\Validator\ProductValidatorPro {}
+if ( class_exists( '\AsanaPlugins\WooCommerce\SaleBadgesPro\Validator\ProductValidator' ) ) {
+	class ProductValidator extends \AsanaPlugins\WooCommerce\SaleBadgesPro\Validator\ProductValidator {}
 } else {
 	class ProductValidator extends BaseProductValidator {}
 }
