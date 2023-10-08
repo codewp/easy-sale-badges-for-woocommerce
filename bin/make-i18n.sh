27b7c18cce13f06aa0200f98befa81548a448e0e
Taher Atashbar <tah3r.a@gmail.com>
2023-10-08 19:09:35 +0800

    Update build-zip. Update make-i18n Update gitignore.

--- bin/make-i18n.sh
+++ bin/make-i18n.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Check for required version
 WPCLI_VERSION=`wp cli version | cut -f2 -d' '`
@@ -8,18 +8,9 @@ if [ ${WPCLI_VERSION:0:1} -lt "2" -o ${WPCLI_VERSION:0:1} -eq "2" -a ${WPCLI_VER
 fi
 
 # Make the JSON files
-wp i18n make-pot ./ languages/asnp-easy-sale-badge.pot
+wp i18n make-pot ./ languages/asnp-easy-sale-badge.pot --exclude=assets/js/dev/*,build/*
 # Make admin script json file.
-echo "Generating admin translation json file..."
-wp i18n make-pot ./assets/js/admin/admin/ languages/pot-file.pot --include="index.js" --domain=asnp-easy-sale-badge
-cp ./languages/pot-file.pot ./languages/po-file.po
+echo "Generating translation json files..."
+cp ./languages/asnp-easy-sale-badge.pot ./languages/asnp-easy-sale-badge.po
 wp i18n make-json languages --no-purge
-cp ./languages/po-file-*.json ./languages/asnp-easy-sale-badge-locale-asnp-whatsapp-admin.json
-rm ./languages/po-file*.po && rm ./languages/pot-file*.pot && rm ./languages/po-file*.json
-# Make whatsapp script json file.
-echo "Generating whatsapp translation json file..."
-wp i18n make-pot ./assets/js/whatsapp/ languages/pot-file.pot --include="index.js" --domain=asnp-easy-sale-badge
-cp ./languages/pot-file.pot ./languages/po-file.po
-wp i18n make-json languages --no-purge
-cp ./languages/po-file-*.json ./languages/asnp-easy-sale-badge-locale-asnp-easy-sale-badge.json
-rm ./languages/po-file*.po && rm ./languages/pot-file*.pot && rm ./languages/po-file*.json
+rm ./languages/asnp-easy-sale-badge.po
