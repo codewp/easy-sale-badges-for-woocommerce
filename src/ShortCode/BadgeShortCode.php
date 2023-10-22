3408df82dccf44913b3721e70deeea074705b5f3
Taher Atashbar <tah3r.a@gmail.com>
2023-10-22 19:02:04 +0800

    Add shortcode.

--- src/ShortCode/BadgeShortCode.php
+++ src/ShortCode/BadgeShortCode.php
@@ -4,13 +4,26 @@ namespace AsanaPlugins\WooCommerce\SaleBadges\ShortCode;
 
 defined( 'ABSPATH' ) || exit;
 
+use function AsanaPlugins\WooCommerce\SaleBadges\display_sale_badges;
+use function AsanaPlugins\WooCommerce\SaleBadges\string_to_bool;
+
 class BadgeShortCode {
 
 	public static function output( $atts ) {
-		$atts = shortcode_atts( array( 'id' => 0 ), $atts, 'asnp_chat' );
-		if ( 0 >= absint( $atts['id'] ) ) {
+		$atts = shortcode_atts( array( 'product_id' => 0, 'hide' => 0 ), $atts, 'asnp_badge' );
+		if ( 0 < absint( $atts['product_id'] ) ) {
+            $product = wc_get_product( $atts['product_id'] );
+        } else {
+            global $product;
+        }
+
+		if ( ! $product ) {
 			return '';
 		}
+
+		$hide = string_to_bool( $atts['hide'] );
+
+		return display_sale_badges( $product, $hide, true );
 	}
 
 }
