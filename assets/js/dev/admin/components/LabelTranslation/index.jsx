4b0a814c7e24b906904393372fdcc00eecbaf474
prswzz <pzmgk10@gmail.com>
2024-02-06 12:35:57 +0330

    Show Label

--- assets/js/dev/admin/components/LabelTranslation/index.jsx
+++ assets/js/dev/admin/components/LabelTranslation/index.jsx
@@ -216,53 +216,59 @@ const LabelTranslation = ( { show, setShow, labelTranslate } ) => {
 							</div>
 							{ labelTranslate &&
 								Object.entries( labelTranslate ).map(
-									( [ lang, translation ], index ) => (
-										<div
-											key={ index }
-											className="asnp-w-full asnp-ml-2 asnp-flex asnp-mt-4"
-										>
-											<div className="asnp-w-[15rem] asnp-mt-1 asnp-h-10 asnp-p-2 asnp-text-lg  asnp-border-b-2 asnp-border-indigo-600">
-												<p>
-													{ translationLang.find(
-														( item ) =>
-															item.value === lang
-													)?.label || lang }
-												</p>
-											</div>
-											<div className="asnp-ml-6">
-												<input
-													type="text"
-													className="asnp-block asnp-text-field !asnp-w-[20rem]"
-													value={ translation }
-													onChange={ ( e ) =>
-														updateTranslation(
-															lang,
-															e.target.value
+									( [ lang, translation ], index ) => {
+										const foundLang = translationLang.find(
+											( item ) => item.lang === lang
+										);
+										return (
+											<div
+												key={ index }
+												className="asnp-w-full asnp-ml-2 asnp-flex asnp-mt-4"
+											>
+												<div className="asnp-w-[15rem] asnp-mt-1 asnp-h-10 asnp-p-2 asnp-text-lg  asnp-border-b-2 asnp-border-indigo-600">
+													<p>
+														{ foundLang
+															? foundLang.label
+															: lang }
+													</p>
+												</div>
+												<div className="asnp-ml-6">
+													<input
+														type="text"
+														className="asnp-block asnp-text-field !asnp-w-[20rem]"
+														value={ translation }
+														onChange={ ( e ) =>
+															updateTranslation(
+																lang,
+																e.target.value
+															)
+														}
+													/>
+													<label className="asnp-block asnp-space-y-1 asnp-text-gray-500">
+														<p className="asnp-text-xs asnp-text-gray-400">
+															{ __(
+																'Edit Translate Label.',
+																'easy-sale-badges-for-woocommerce'
+															) }
+														</p>
+													</label>
+												</div>
+												<button
+													className="asnp-mb-4 asnp-ml-3 asnp-mt-2 asnp-btn-delete asnp-py-1 asnp-h-[2rem] asnp-w-[4rem] asnp-font-semibold asnp-shadow-md asnp-rounded-lg focus:asnp-shadow-none"
+													onClick={ () =>
+														deleteTranslation(
+															lang
 														)
 													}
-												/>
-												<label className="asnp-block asnp-space-y-1 asnp-text-gray-500">
-													<p className="asnp-text-xs asnp-text-gray-400">
-														{ __(
-															'Edit Translate Label.',
-															'easy-sale-badges-for-woocommerce'
-														) }
-													</p>
-												</label>
+												>
+													{ __(
+														'Delete',
+														'easy-sale-badges-for-woocommerce'
+													) }
+												</button>
 											</div>
-											<button
-												className="asnp-mb-4 asnp-ml-3 asnp-mt-2 asnp-btn-delete asnp-py-1 asnp-h-[2rem] asnp-w-[4rem] asnp-font-semibold asnp-shadow-md asnp-rounded-lg focus:asnp-shadow-none"
-												onClick={ () =>
-													deleteTranslation( lang )
-												}
-											>
-												{ __(
-													'Delete',
-													'easy-sale-badges-for-woocommerce'
-												) }
-											</button>
-										</div>
-									)
+										);
+									}
 								) }
 						</div>
 					</div>
