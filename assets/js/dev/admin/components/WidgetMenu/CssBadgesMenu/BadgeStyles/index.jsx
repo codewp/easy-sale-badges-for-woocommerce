9894598557a325311a2261bd4f2877df6da453be
prswzz <pzmgk10@gmail.com>
2024-02-06 15:28:20 +0330

    Fix Css

--- assets/js/dev/admin/components/WidgetMenu/CssBadgesMenu/BadgeStyles/index.jsx
+++ assets/js/dev/admin/components/WidgetMenu/CssBadgesMenu/BadgeStyles/index.jsx
@@ -11,7 +11,7 @@ const BadgeStyles = ( props ) => {
 				<div className="asnp-w-[25rem] asnp-text-lg asnp-font-semibold">
 					{ __( 'Label Styles', 'easy-sale-badges-for-woocommerce' ) }
 				</div>
-				<div className="asnp-relative asnp-mb-3 asnp-mt-3 asnp-h-[18rem] asnp-pb-2 asnp-pt-2 asnp-flex asnp-flex-col asnp-border-[1px] asnp-border-gray-300 asnp-rounded-2xl">
+				<div className="asnp-relative asnp-w-[52rem] asnp-mb-3 asnp-mt-3 asnp-h-[18rem] asnp-pb-2 asnp-pt-2 asnp-flex asnp-flex-col asnp-border-[1px] asnp-border-gray-300 asnp-rounded-2xl">
 					<div className="asnp-w-full asnp-flex asnp-py-2 asnp-space-x-8 asnp-ml-4">
 						<div
 							className={
