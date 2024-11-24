de0819083214e3d1af673a3e06732bbdde9ceacc
prswzz <pzmgk10@gmail.com>
2024-11-25 00:00:57 +0330

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'asanaplugins/easy-sale-badges',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '238a00c940c8a81e7533d273ff025564f9186c56',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'asanaplugins/easy-sale-badges' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '238a00c940c8a81e7533d273ff025564f9186c56',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
